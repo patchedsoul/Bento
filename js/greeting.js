c86f4f1b16cd525191afb31cec623cde05c8fc0c
Jonathan Baginski <jonathan@patchedsoul.com>
2020-09-25 05:12:35 -0400

    Update greeting.js

--- js/greeting.js
+++ js/greeting.js
@@ -3,13 +3,13 @@ var Hr = today.getHours();
 
 // Here you can change the greetings and your name
 var name = 'Jonathan';
-var lateTxt = 'Go to Sleep! ';
+var earlyTxt = 'Greetings ';
 var morningTxt = 'Good morning! ';
 var afterTxt = 'Good afternoon ';
 var evenTxt = 'Good evening ';
 
 if (Hr >= 0 && Hr < 6) {
-    document.getElementById('greetings').innerText = lateTxt + name;
+    document.getElementById('greetings').innerText = earlyTxt + name;
 } else if (Hr >= 6 && Hr < 12) {
     document.getElementById('greetings').innerText = morningTxt + name;
 } else if (Hr >= 12 && Hr < 17) {
